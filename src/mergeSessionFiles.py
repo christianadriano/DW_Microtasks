7941c8837732a93b48d4ee6c5d17e93ab0ce88a7
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2018-03-15 17:09:05 +0100

    Initial commit

---
+++ src/mergeSessionFiles.py
@@ -0,0 +1,17 @@
+'''
+Created on Mar 15, 2018
+
+@author: Chris
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        

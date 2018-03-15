cf100bc29ffdc688e8ee67dde3dc2b82725c8c72
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2018-03-15 19:34:20 +0100

    Loading file in a nested dictionary

--- src/mergeSessionFiles.py
+++ src/mergeSessionFiles.py
@@ -3,6 +3,8 @@ Created on Mar 15, 2018
 
 @author: Chris
 '''
+from idlelib.browser import file_open
+from fileinput import filelineno
 
 class MyClass(object):
     '''
@@ -10,8 +12,45 @@ class MyClass(object):
     '''
 
 
-    def __init__(self, params):
+    def __init__(self):
         '''
         Constructor
         '''
+        file_header = ['time_stamp','event','workerID','microtaskID','fileName','question','answer','duration','explanation']
+        line = {0:file_header}
+        file_lines = [line]
+        #Load file into a dictionary 
+        with open('C://Users//Chris//Dropbox (Personal)//FaultLocalization_Microtasks_data//Experiment-1_2014//session_Run1-Total-25oct.log') as file_object:
+            i=0
+            for line in file_object:
+                i=i+1
+                new_line = {i:line}
+                file_lines.append(new_line) 
+        print(file_lines[0:4])
+        file_object.close()
+
+    
+   # def __cleanUp_BreakExplanations__(file_lines):
+   #      for i in file_lines:
         
+    
+    def __parseLine__(file_line):
+        
+        if(hasNumber(file_line[0])
+        
+        time_stamp = parseTimeStamp(file_line)
+        event = parseEvent(file_line)
+        workerID = parseWorkerID(file_line)
+        microtaskID = parseMicrotaskID(file_line)
+        question = parseQuestion(file_line)
+        answer = parseQuestion(file_line)
+        duration = parseDuration(file_line)
+        explanation = parseExplanation(file_line)
+        
+        
+        
+    def hasNumbers(inputString):
+...     return any(char.isdigit() for char in inputString)
+
+myObject = MyClass()
+myObject.__init__()

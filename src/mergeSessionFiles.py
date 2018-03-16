b143f2cc9d83618db61df63ac835486fa4c1962f
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2018-03-16 16:56:19 +0100

    reorganized functions, created consolidatedLines

--- src/mergeSessionFiles.py
+++ src/mergeSessionFiles.py
@@ -4,8 +4,7 @@ Created on Mar 15, 2018
 @author: Chris
 '''
 from idlelib.browser import file_open
-from fileinput import filelineno
-from Lib import tokenize
+import re
 
 class MyClass(object):
     '''
@@ -13,32 +12,39 @@ class MyClass(object):
     '''
 
 
-    def __init__(self):
+    def __main__(self):
         '''
         Constructor
         '''
         file_header = ['time_stamp','event','workerID','microtaskID','fileName','question','answer','duration','explanation']
-        line = {0:file_header}
-        file_lines = [line]
+        file_lines = [{0:file_header}]
         #Load file into a dictionary 
-        with open('C://Users//Chris//Dropbox (Personal)//FaultLocalization_Microtasks_data//Experiment-1_2014//session_Run1-Total-25oct.log') as file_object:
+        root = 'C://Users//Chris//Dropbox (Personal)//FaultLocalization_Microtasks_data//'
+        path2014 = root + 'Experiment-1_2014//'
+        file_name = 'session_Run1-Total-25oct.log'
+        file_path = path2014 + fileName
+        file_lines = self.loadFile(file_lines,file_path)
+        file_lines = __consolidateBrokenExplanations__(file_lines)
+        
+
+    def loadFile(self,file_lines, file_path):
+        with open(file_path) as file_object:
             i=0
             for line in file_object:
                 i=i+1
                 new_line = {i:line}
                 file_lines.append(new_line) 
-        print(file_lines[0:4])
+        #print(file_lines[0:4])
         file_object.close()
+        return file_lines
+    
+  
+    def hasNumbers(self,inputString):
+       return any(char.isdigit() for char in inputString)
 
-    
-   # def __cleanUp_BreakExplanations__(file_lines):
-   #      for i in file_lines:
-        
-    
-    def __parseLine__(file_line):
-        
-        if(hasNumber(file_line[0]):
-            tokens = re.sprint(';',file_line)
+
+    def __parseLinesToDictionary__(self,file_lines):
+            tokens = re.split(';',file_lines)
             time_stamp_event = tokens[0]
             time_stamp = time_stamp_event[:12]
             event = re.split('\s-\s\=',time_stamp_event)
@@ -50,12 +56,23 @@ class MyClass(object):
             duration = tokens[6]
             explanation = tokens[7]
             dictionaryLine = {}
-        else:
                 
-        
-        
-    def hasNumbers(inputString):
-...     return any(char.isdigit() for char in inputString)
-
+    def __consolidateBrokenExplanations__(self,file_lines):
+        accumulatedLine=''#initialize and empty line
+        i=-1
+        previousIndex=-1
+        for line in file_lines: 
+             i=i+1
+            if(not hasNumber(file_line[0]):
+               if previousIndex!=-1:
+                   previousIndex=i
+               processed_lines[previousIndex] = processed_lines[previousIndex]+line
+            else:
+                 previosIndex=-1 #stop accumulating extra explanation lines
+                 processed_lines = line
+            return processed_lines
+                    
+   
+         
 myObject = MyClass()
 myObject.__init__()

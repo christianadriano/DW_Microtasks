7edb499642866867e50ad0a71120a49e34ea226c
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2018-03-20 18:30:55 +0100

    fixed and renamed test

--- test/test_MergeSessionFiles.py
+++ test/test_MergeSessionFiles.py
@@ -1,21 +1,21 @@
-import unittest
+'''
+Created on Mar 20, 2018
 
+@author: Chris
+'''
+import unittest
 from MergeSessionFiles import SessionLoader
 
+class Test(unittest.TestCase):
 
-class TestSessionLoader(unittest.TestCase):
-    '''
-    Test session loader methods
-    '''
-
-    def __init__(self):
+    def test_consolidate_lines(self):
         self.sessionLoader = SessionLoader()
-        
-    def test_consolidate(self):
         file_path = "C://Users//Chris//Documents//GitHub//DW_Microtasks//test//testData.txt"
         file_lines = self.sessionLoader.load_file(file_path)
         file_lines = self.sessionLoader.consolidate_broken_explanations(file_lines)
-        file_lines
         self.assertEqual(3,file_lines.__len__())
 
-unittest.main()
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()

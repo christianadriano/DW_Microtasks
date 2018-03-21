a09107af40d50802a6346da4baf458cd89287027
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2018-03-21 10:34:05 +0100

    test load files

--- test/test_MergeSessionFiles.py
+++ test/test_MergeSessionFiles.py
@@ -8,11 +8,19 @@ from MergeSessionFiles import SessionLoader
 
 class Test(unittest.TestCase):
 
-    def test_consolidate_lines(self):
+    def test_load_file(self):
+        self.sessionLoader = SessionLoader()
+        file_path = "C://Users//Chris//Documents//GitHub//DW_Microtasks//test//testData.txt"
+        file_lines = self.sessionLoader.load_file(file_path)
+        print(file_lines)
+        self.assertEqual(4,file_lines.__len__())
+
+    def consolidate_lines(self):
         self.sessionLoader = SessionLoader()
         file_path = "C://Users//Chris//Documents//GitHub//DW_Microtasks//test//testData.txt"
         file_lines = self.sessionLoader.load_file(file_path)
         file_lines = self.sessionLoader.consolidate_broken_explanations(file_lines)
+        print(file_lines)
         self.assertEqual(3,file_lines.__len__())
 
 

3e442a56c9a21eed841f7e5230ef1678da292ba0
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2018-08-13 20:59:17 +0200

    Created a function to extract data from the consent file.

--- src/mergeSessionFiles.py
+++ src/mergeSessionFiles.py
@@ -10,6 +10,7 @@ Processes SessionLog and ConsentLog Files
 #from idlelib.browser import file_open
 import re
 from util.FileReaderWriter import FileReaderWriter  
+from _overlapped import NULL
 
 class SessionLoader:
     '''
@@ -27,24 +28,32 @@ class SessionLoader:
         """process the two files"""
         file_name_1= self.root + "session_Run1-Total-25oct.log"
         file_name_2= self.root + "session_Run2-28oct.log"
-        tuple_lines = self.run("C://Users//Chris//Documents//GitHub//DW_Microtasks//test//testData.txt",suffix='1') #file_name="session_Run1-Total-25oct.log",suffix='1')
-        tuple_lines = tuple_lines+ self.run("C://Users//Chris//Documents//GitHub//DW_Microtasks//test//testData_2.txt", suffix='2') #file_name="session_Run2-28oct.log", suffix='2')
+        tuple_lines = self.run("C://Users//Chris//Documents//GitHub//DW_Microtasks//test//testData.txt",
+                               "C://Users//Chris//Documents//GitHub//DW_Microtasks//test//consentTestData.txt",
+                               suffix='1') #file_name="session_Run1-Total-25oct.log",suffix='1')
         
+        """tuple_lines = tuple_lines + self.run("C://Users//Chris//Documents//GitHub//DW_Microtasks//test//testData_2.txt", suffix='2') #file_name="session_Run2-28oct.log", suffix='2')
+        """
         writer = FileReaderWriter()
         writer.write_session_log_arff(tuple_lines, 
                                    self.output+'FailuireUnderstanding_Crowd_1.arff',
                                     self.get_header_arff())
 
-    def run(self,file_name_path,suffix):
+    def run(self,session_file_name_path,consent_file_name_path, suffix):
         """ coordinate the loading, Cleaning, Formating, and Writing of Session Files"""
         """ file_name of the session log data, suffix is either 1 or 2, to indicate from which file this register came"""
-        file_lines = ['time_stamp','event','workerID','microtaskID','fileName','question','answer','duration','explanation']
+        session_file_lines = ['time_stamp','event','worker_id','session_id', 'microtask_id','file_name','question','answer','duration','explanation']
         #Load file into a dictionary 
-        file_lines = self.load_file(file_name_path)
-        file_lines = self.consolidate_broken_lines(file_lines)
-        tuple_lines = self.parse_all_to_dictionary(file_lines, suffix)
+        session_file_lines = self.load_session_file_lines(session_file_name_path)
+        worker_data = self.load_consent_file(consent_file_name_path,suffix)
+        tuple_lines = self.parse_all_to_dictionary(session_file_lines, worker_data, suffix)
         #print file_lines to file
         return (tuple_lines)
+
+    def load_session_file_lines(self,file_path):
+        file_lines = self.load_file(file_path)
+        file_lines = self.consolidate_broken_lines(file_lines)
+        return file_lines
 
     def load_file(self,file_path):
         """Read a file and writes the content in a list of dictionaries [{lineNuber:LineContent}]"""
@@ -71,7 +80,55 @@ class SessionLoader:
                 processed_lines.append(line.strip("\n"))
                 i=i+1
         return processed_lines
-                    
+                 
+    def load_consent_file(self,consent_file_path, suffix):
+        """Load all the data from each worker on a dictionary that can be queried later on""" 
+        
+        """for each worker_id, keeps the data about consent, skill test, and survey"""
+        consent_dictionnary = {} 
+        consent_file_lines = self.load_file(consent_file_path)
+        consent_file_lines = self.consolidate_broken_lines(consent_file_lines) 
+        for line in consent_file_lines:
+            parsed_line = self.parse_consent_line_to_dictionary(line,suffix) 
+            key = parsed_line["worker_id"]
+            print()
+            print(parsed_line)
+            print(key)
+            if(key in consent_dictionnary):
+                existing_dictionary = consent_dictionnary[key]  
+                existing_dictionary.update(parsed_line)
+                consent_dictionnary[key] = existing_dictionary
+            else:
+                consent_dictionnary[key] = parsed_line
+        return(consent_dictionnary)
+           
+    def parse_consent_line_to_dictionary(self,line,suffix):
+        """parse the line into a dictionary"""
+        tokens = re.split(';',line)    
+        time_stamp_event = tokens[0]
+        time_stamp = time_stamp_event[:12]
+        event = (re.split('\=',tokens[0])[1]).strip()
+        worker_ID = re.split('\=',tokens[1])[1]+"_"+suffix
+        tuple_line={"time_stamp":time_stamp,"event":event,"worker_id":worker_ID}
+        if(event=="CONSENT"):
+            tuple_line["consent_date"] = re.split('\=',tokens[2])[1] 
+        elif(event=="SKILLTEST"):
+            tuple_line["test1"] = re.split('\=',tokens[2])[1]
+            tuple_line["test2"] = re.split('\=',tokens[3])[1]
+            tuple_line["test3"] = re.split('\=',tokens[4])[1]
+            tuple_line["test4"] = re.split('\=',tokens[5])[1]
+            tuple_line["grade"] =  re.split('\=',tokens[6])[1]
+            tuple_line["testDuration"] = re.split('\=',tokens[7])[1]
+        elif(event=="SURVEY"):
+            tuple_line["session_id"] = re.split('\=',tokens[2])[1]
+            tuple_line["feedback"] = self.replace_commas(re.split('\=',tokens[3])[1])    
+            tuple_line["gender"] = re.split('\=',tokens[4])[1]
+            tuple_line["years_programming"] = re.split('\=',tokens[5])[1]
+            tuple_line["difficulty"] = re.split('\=',tokens[6])[1] 
+            tuple_line["country"] = re.split('\=',tokens[7])[1]  
+            tuple_line["age"] = re.split('\=',tokens[8])[1]   
+        return (tuple_line)      
+           
     def match_start_tuple(self,first_eight_characters):
         """tests if the string corresponds to the beginning of new tuple NN:NN:NN"""
         isNewTuple = False
@@ -81,11 +138,13 @@ class SessionLoader:
             isNewTuple = False
         return isNewTuple
    
-    def parse_all_to_dictionary(self,file_lines,suffix):
+    def parse_all_to_dictionary(self,file_lines,worker_data,suffix):
         """parse all lines into dictionary tuples, return a list of these tuples"""
         parsed_lines=[]
         for line in file_lines:
-            parsed_lines.append(self.parse_line_to_dictionary(line, suffix))
+            newLine = self.parse_line_to_dictionary(line, suffix)
+            """newLine = add_worker_data(newLine["worker_id"],newLine,suffix)"""
+            parsed_lines.append(newLine)
         return parsed_lines
     
     def parse_line_to_dictionary(self,line,suffix):

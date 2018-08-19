73bee2e1fb1efd1c9e7202ccf524907f6c09cd16
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2018-08-20 00:03:49 +0200

    cleaned up code

--- parserFactory/Parser.py
+++ parserFactory/Parser.py
@@ -127,8 +127,8 @@ class Parser_Run2(Parser):
         """parse the line into a dictionary"""
         tokens = re.split(self.separator1,line)    
         event = tokens[1].strip()       
-        worker_ID = tokens[3]+"_"+self.suffix
-        tuple_line={"worker_id":worker_ID} #need this to find index the tuple
+        worker_id = tokens[3].strip()+"_"+self.suffix
+        tuple_line={"worker_id":worker_id} #need this to find index the tuple
         if(event=="CONSENT"):
             tuple_line["consent_date"] = tokens[5].strip()
         elif(event=="SKILLTEST"):
@@ -139,12 +139,12 @@ class Parser_Run2(Parser):
             tuple_line["grade"] = tokens[13].strip()
             tuple_line["testDuration"] = tokens[15].strip()
         elif(event=="SURVEY"):
-                tuple_line["feedback"] = tokens[7].replace(",",";")
-                tuple_line["gender"] = tokens[9]
-                tuple_line["years_programming"] = tokens[11]
-                tuple_line["difficulty"] = tokens[13]
-                tuple_line["country"] = tokens[15]
-                tuple_line["age"] = tokens[17]  
+            tuple_line["feedback"] = tokens[7].replace(",",";")
+            tuple_line["gender"] = tokens[9]
+            tuple_line["years_programming"] = tokens[11]
+            tuple_line["difficulty"] = tokens[13]
+            tuple_line["country"] = tokens[15]
+            tuple_line["age"] = tokens[17]  
         return (tuple_line)       
      
     
@@ -167,9 +167,7 @@ class Parser_Run2(Parser):
             index = line.find("explanation%") + "explanation%".__len__()
             tuple_line["explanation"] = line[index:].replace(",",";")          
         return (tuple_line)
-    
-
-    
+        
 class Parser_Run1(Parser):
     '''
     Parser for the run 1 of experiment 1
@@ -188,10 +186,9 @@ class Parser_Run1(Parser):
         tokens = re.split(self.separator1,line)    
         time_stamp_event = tokens[0]
         time_stamp = time_stamp_event[:12]
-        event = tokens[0].rsplit(self.separator2)[1]
-#         event = self.extract_event(line,self.separator1, self.separator2)        
-        worker_ID = re.split(self.separator2,tokens[1])[1]+"_"+self.suffix
-        tuple_line={"worker_id":worker_ID} #need this to find index the tuple
+        event = tokens[0].rsplit(self.separator2)[1].strip()
+        worker_id = re.split(self.separator2,tokens[1])[1].strip()+"_"+self.suffix
+        tuple_line={"worker_id":worker_id} #need this to find index the tuple
         if(event=="CONSENT"):
             tuple_line["consent_date"] = (re.split(self.separator2,tokens[2])[1]).strip()
         elif(event=="SKILLTEST"):
@@ -203,22 +200,18 @@ class Parser_Run1(Parser):
             tuple_line["testDuration"] = (re.split(self.separator2,tokens[7])[1]).strip()
         elif(event=="SURVEY"):
             tcount = 3
-            try:
-                results = self.extract_feedback(tokens,3,"Gender=",self.separator2)
-                tuple_line["feedback"] = results[0]
-                tcount = results[1] + 1 
-                tuple_line["gender"] = re.split(self.separator2,tokens[tcount])[1]
-                tcount += 1
-                tuple_line["years_programming"] = re.split(self.separator2,tokens[tcount])[1]
-                tcount += 1
-                tuple_line["difficulty"] = re.split(self.separator2,tokens[tcount])[1] 
-                tcount += 1
-                tuple_line["country"] = re.split(self.separator2,tokens[tcount])[1]  
-                tcount += 1
-                tuple_line["age"] = re.split(self.separator2,tokens[tcount])[1]  
-            except:
-                print("   Exception in SURVEY tuple, tcount= "+ str(tcount))
-                print("time_stamp = "+ time_stamp)
+            results = self.extract_feedback(tokens,3,"Gender=",self.separator2)
+            tuple_line["feedback"] = results[0]
+            tcount = results[1] + 1 
+            tuple_line["gender"] = re.split(self.separator2,tokens[tcount])[1]
+            tcount += 1
+            tuple_line["years_programming"] = re.split(self.separator2,tokens[tcount])[1]
+            tcount += 1
+            tuple_line["difficulty"] = re.split(self.separator2,tokens[tcount])[1] 
+            tcount += 1
+            tuple_line["country"] = re.split(self.separator2,tokens[tcount])[1]  
+            tcount += 1
+            tuple_line["age"] = re.split(self.separator2,tokens[tcount])[1]
         return (tuple_line)      
     
     def extract_feedback(self,tokens,index,endToken,separator):

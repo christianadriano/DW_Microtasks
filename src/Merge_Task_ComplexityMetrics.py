a68d2f13cc13e75211ee2fd92ab4ba26a33a1f8a
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-10-16 17:32:37 +0200

    Update Merge_Task_ComplexityMetrics.py
    Merged tasks and complexity and wrote to a cvs file

--- src/Merge_Task_ComplexityMetrics.py
+++ src/Merge_Task_ComplexityMetrics.py
@@ -28,7 +28,7 @@ class Merge_Task_ComplexityMetrics(object):
         self.df_tasks_E2 = pd.DataFrame(array_tasks_E2)
         #print(dataset_tasks_E2['attributes'])
         #need to extract the header, because the arf.loads brings only data....        
-        tasks_E2_header = np.take(array_tasks_E2['attributes'], [0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,62])
+        tasks_E2_header = np.take(dataset_tasks_E2['attributes'], [0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,62])
         self.df_tasks_E2.columns = tasks_E2_header
         
         #COMPLEXITY METRICS
@@ -37,13 +37,17 @@ class Merge_Task_ComplexityMetrics(object):
 
     def _left_join_task_complexity_E2(self):
         '''
-        do a left join of the tasks with their correspoding complexity metrics
+        do a left join of the tasks with their corresponding complexity metrics
         for experiment 2
         '''
-        print (pd.merge(self.df_tasks_E2, 
+        self.df_complexity_E2 = self.df_complexity_E2.drop(['file_name', 'type'],axis=1) 
+        self.df_tasks_E2['microtask_id'] = self.df_tasks_E2['microtask_id'].astype(int)
+        df_results_E2 =pd.merge(self.df_tasks_E2, 
                         self.df_complexity_E2, 
-                        on=['file_name','microtask_id'], 
-                        how='left'))
+                        on=['microtask_id'], 
+                        how='left') 
+        print (df_results_E2)
+        df_results_E2.to_csv(self.complexity_file_path + 'merged_tasks_complexity_E2.csv')
 
     def _append_metrics_E1(self):
         '''
@@ -61,9 +65,9 @@ class Merge_Task_ComplexityMetrics(object):
         self.file_tasks_E2 = task_file_path + 'consolidated_Final_Experiment_2.arff'
         self.file_tasks_E1 = task_file_path + 'consolidated_Final_Experiment_1.arff'
         
-        complexity_file_path = 'C://Users//Christian//Documents//GitHub//Complexity_Metrics//output//'
-        self.file_complexity_E2 = complexity_file_path + 'microtask_complexity_E2.csv'
-        self.file_complexity_E1 = complexity_file_path + 'microtask_complexity_E1.csv'
+        self.complexity_file_path = 'C://Users//Christian//Documents//GitHub//Complexity_Metrics//output//'
+        self.file_complexity_E2 = self.complexity_file_path + 'microtask_complexity_E2.csv'
+        self.file_complexity_E1 = self.complexity_file_path + 'microtask_complexity_E1.csv'
         
 #CONTROLLER CODE
 merger = Merge_Task_ComplexityMetrics()

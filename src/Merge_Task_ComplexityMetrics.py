232ad714f6650840490c5aaf187f896b4e5f1b57
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-10-16 16:05:12 +0200

    Update Merge_Task_ComplexityMetrics.py
    Added header to dataframe

--- src/Merge_Task_ComplexityMetrics.py
+++ src/Merge_Task_ComplexityMetrics.py
@@ -8,8 +8,9 @@ There are four metrics: LOC, Cyclomatic complexity, Size Char, Halstead Length, 
 
 @author: Christian
 '''
-from scipy.io import arff
 import pandas as pd
+import arff as arf
+import numpy as np
 
 class Merge_Task_ComplexityMetrics(object):
     '''
@@ -21,14 +22,28 @@ class Merge_Task_ComplexityMetrics(object):
         Load task outcomes file into a dataframe
         '''
         
-        array_tasks_E2=pd.arff.loadarff(self.file_tasks_E2)
-        df_tasks_E2 = pd.DataFrame(array_tasks_E2[0])
-        df_complexity = pd.read_csv(self.file_tasks_E2)
+        #TASKS
+        dataset_tasks_E2 = arf.loads(open(self.file_tasks_E2, 'rt'))
+        array_tasks_E2 = np.array(dataset_tasks_E2['data'])
+        self.df_tasks_E2 = pd.DataFrame(array_tasks_E2)
+        #print(dataset_tasks_E2['attributes'])
+        #need to extract the header, because the arf.loads brings only data....        
+        tasks_E2_header = np.take(array_tasks_E2['attributes'], [0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,62])
+        self.df_tasks_E2.columns = tasks_E2_header
+        
+        #COMPLEXITY METRICS
+        self.df_complexity_E2 = pd.read_csv(self.file_complexity_E2)
 
-    def _append_metrics_E2(self):
+
+    def _left_join_task_complexity_E2(self):
         '''
-        append metrics for experiment 2 tasks
+        do a left join of the tasks with their correspoding complexity metrics
+        for experiment 2
         '''
+        print (pd.merge(self.df_tasks_E2, 
+                        self.df_complexity_E2, 
+                        on=['file_name','microtask_id'], 
+                        how='left'))
 
     def _append_metrics_E1(self):
         '''
@@ -42,13 +57,15 @@ class Merge_Task_ComplexityMetrics(object):
         Initialize folders 
         '''
         super().__init__()
-        self.file_root ='C://Users//Christian//Documents//GitHub//DW_Microtasks//output//'
-        self.file_tasks_E2 = file_root + 'consolidated_Final_Experiment_2.arff'
-        self.file_tasks_E1 = file_root + 'consolidated_Final_Experiment_1.arff'
-        self.file_complexity_E2 = file_root + ''
-        self.file_complexity_E1 =''
+        task_file_path ='C://Users//Christian//Documents//GitHub//DW_Microtasks//output//'
+        self.file_tasks_E2 = task_file_path + 'consolidated_Final_Experiment_2.arff'
+        self.file_tasks_E1 = task_file_path + 'consolidated_Final_Experiment_1.arff'
         
+        complexity_file_path = 'C://Users//Christian//Documents//GitHub//Complexity_Metrics//output//'
+        self.file_complexity_E2 = complexity_file_path + 'microtask_complexity_E2.csv'
+        self.file_complexity_E1 = complexity_file_path + 'microtask_complexity_E1.csv'
         
-        '''self.file_testInput = 'C://Users//Christian//Documents//GitHub//DW_Microtasks//test//'        
-        
-        
+#CONTROLLER CODE
+merger = Merge_Task_ComplexityMetrics()
+merger._load_E2_files()   
+merger._left_join_task_complexity_E2()
